15eff98f24e91d2a6b35a0907afcaadb396d6a48
Sally <sewelsch@wisc.edu>
2026-04-03 12:24:49 -0500

    homeAgain

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/p202",
+  base: "/p202/",
   plugins: [react()],
   build: {
     outDir: "docs"
