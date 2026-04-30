0fcdbd8850ebd3fea67d646f30647c5aa9b9320e
Sally <sewelsch@wisc.edu>
2026-04-29 23:59:15 -0500

    more

--- src/pages/More.jsx
+++ src/pages/More.jsx
@@ -6,6 +6,7 @@ import MiscCard from '../components/MiscCard';
 function More() {
 	return <>
 		<Container fluid>
+			<p style={{margin:15}}> Here are some of my miscellaneous endeavours.</p>
 			<Row>
 				{miscdata.map(misc => (
 					<Col xs={6} md={4} lg={3} key={misc.id}>
